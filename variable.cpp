5a21d8dc07f91cdf3f493382d77fed6627f09dac
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-12-12 02:35:15 +0600

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=10;
+    float num2=20.33;
+    char myletter='P';
+    string my_text="Math";
+
+    cout<<num1;
+    cout<<"\n";
+
+    cout<<num2;
+
+    cout<<"\n";
+
+    cout<<"My Text is ",cout<<my_text;
+
+
+    return 0;
+}
+
